a1fb41180f6312ce704460f76284a50e38ec411a
jiku1797 <jiku1797@gmail.com>
2026-01-31 13:55:16 +0100

    First commit

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "shot-counter-v1";
+const urlsToCache = [
+    "/",                  // index.html
+    "/index.html",        // make sure path is correct
+    "/manifest.json",
+    "/icons/hockey_icon_120.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
