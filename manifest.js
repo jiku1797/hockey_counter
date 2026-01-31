7e7836559e4a2692caca0dded24f1df68ffe2a8e
jiku1797 <jiku1797@gmail.com>
2026-01-31 15:09:24 +0100

    fixup: icon

--- manifest.js
+++ manifest.js
@@ -1,18 +1,18 @@
 {
   "name": "Shot Counter",
   "short_name": "Shots",
-  "start_url": ".",
+  "start_url": "./",
   "display": "standalone",
   "background_color": "#111111",
   "theme_color": "#111111",
   "icons": [
     {
-      "src": "/hockey_icon_192.png",
+      "src": "hockey_icon_192.png",
       "sizes": "192x192",
       "type": "image/png"
     },
     {
-      "src": "/hockey_icon_512.png",
+      "src": "hockey_icon_512.png",
       "sizes": "512x512",
       "type": "image/png"
     }
